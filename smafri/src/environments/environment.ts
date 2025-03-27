fadf8500383e812bd6681f9e12997ae82ed7304b
Monika Pham <monikapham@MacBook-Air-von-Monika.local>
2025-03-27 11:30:25 +0100

    fix

--- smafri/src/environments/environment.ts
+++ smafri/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://smartfridge-tvaz.onrender.com'
+    apiUrl: 'https://smartfridge-tvaz.onrender.com/api'
   };
   
